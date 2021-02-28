0dce50e9bd3551ea9b08fe9261afd2af8f23f0e2
wykim111 <ksq111@naver.com>
2021-02-28 23:58:55 +0900

    두 수에 대한 더하기 추가  -우영-

--- CoWorkPlace/src/main/Main.java
+++ CoWorkPlace/src/main/Main.java
@@ -2,6 +2,15 @@ package main;
 
 import java.util.Scanner;
 
+class Calculate
+{
+	int add(int n1,int n2)
+	{
+		return n1 + n2;
+	}
+	//마이너스 추가 부탁 
+}
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -9,10 +18,15 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		int num1 ;
 		int num2 ;
+		Calculate cal = new Calculate();
+		
 		
 		num1 = scanner.nextInt();
 		num2 = scanner.nextInt();
 		
+		System.out.println(num1+ "," + num2);
+		
+		System.out.println("더하기 = " + cal.add(num1, num2));
 				
 	}
 
