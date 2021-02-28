3443b33a526ce93c9499f2cb4bdebd4a93b2178d
enterblack <gkswnsgml_@naver.com>
2021-02-28 23:36:10 +0900

    calMain

--- CoWorkPlace/src/main/Main.java
+++ CoWorkPlace/src/main/Main.java
@@ -1,11 +1,19 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner(System.in);
+		int num1 ;
+		int num2 ;
 		
-
+		num1 = scanner.nextInt();
+		num2 = scanner.nextInt();
+		
+				
 	}
 
 }
