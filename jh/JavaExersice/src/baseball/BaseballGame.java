a8d4962240c0b61728bf1a550aa84b2006e36c74
enterblack <gkswnsgml_@naver.com>
2021-03-20 22:34:59 +0900

    baseball game add Ball function

--- jh/JavaExersice/src/baseball/BaseballGame.java
+++ jh/JavaExersice/src/baseball/BaseballGame.java
@@ -28,20 +28,34 @@ public class BaseballGame {
 			for(int i=0 ; i<answerTemp.length() ; i++) {
 				sumitArray[i] = answerTemp.charAt(i);
 			}
-			int strike = countCorrectNumber(answer, sumitArray);
+			int strike = countStrikeNumber(answer, sumitArray);
+			int ball = countBalllNumber(answer, sumitArray);
 			// 정답일경우 즉 3스트라이크일경우 정답알림과 프로그램종료
 			if(strike == 3){
 				System.out.println("정답입니다. 축하드립니다!");
 				System.exit(0);
 			}
-			System.out.println(strike+"스트라이크입니다. 좀만더 해보세요!");
+			System.out.println(strike+"스트라이크 " + ball  + "볼입니다. 좀만더 해보세요!");
 			
 		}
 	}
-
-	private static int countCorrectNumber(char[] answer, char[] sumitArray) {
-		int strike = 0;
+	private static int countBalllNumber(char[] answer, char[] sumitArray) {
 		int ball = 0;
+		for(int i=0 ; i<answer.length ; i++) {
+			for(int k=0 ; k<sumitArray.length ; k++) {
+				if(i==k) {
+					continue;
+				}
+				if(answer[i] == sumitArray[k]) {
+					ball++;
+				}
+			}
+		}
+		return ball;
+	}
+	//같은데이터에 같은 자리면 strike
+	private static int countStrikeNumber(char[] answer, char[] sumitArray) {
+		int strike = 0;
 		for(int i=0 ; i<answer.length ; i++) {
 			if(answer[i] == sumitArray[i]) {
 				strike++;
@@ -49,7 +63,7 @@ public class BaseballGame {
 		}
 		return strike;
 	}
-
+	//같은 데이터이지만 자리수가 다르면 ball
 
 	
 	
