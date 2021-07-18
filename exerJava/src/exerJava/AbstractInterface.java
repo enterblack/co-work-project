27b97a23e3b4828e3b2850ab49097df4fe54baa2
wykim111 <ksq111@naver.com>
2021-07-19 00:00:28 +0900

    interface 일부 구현 ver01

---
+++ exerJava/src/exerJava/AbstractInterface.java
@@ -0,0 +1,16 @@
+package exerJava;
+
+abstract class PersonalNumberStorage
+{
+	public abstract void addPersonalInfo(String name,String perNum);
+	public abstract String searchName(String perNum);
+}
+
+
+
+public class AbstractInterface {
+	public static void main(String[] args)
+	{
+		
+	}
+}
